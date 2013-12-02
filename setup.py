7fb83dfe01e9ad70d3e4d435c5de1647c1689028
Alfredo Deza <alfredo@deza.pe>
2013-12-02 15:34:18 -0500

    fix imports in setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-	name='helga-excuses',
-	use_hg_version=True,
-	author="Alfredo Deza",
-	author_email="contact@deza.pe",
-	url="https://github.com/alfredodeza/helga-excuses",
-	packages=setuptools.find_packages(),
-	include_package_data=True,
-	zip_safe=True,
+    name='helga-excuses',
+    use_hg_version=True,
+    author="Alfredo Deza",
+    author_email="contact@deza.pe",
+    url="https://github.com/alfredodeza/helga-excuses",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True,
     install_requires=[
         'BeautifulSoup',
         'requests',
+    ],
     entry_points = dict(
         helga_handlers = [
             'excuses = helga_excuses.excuses:ExcusesExtension',
